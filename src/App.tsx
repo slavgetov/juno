410ee831780c254b1f4d8a14176603d0ae1ca2c4
slavgetov <preslavcodes@abv.bg>
2025-01-19 14:34:43 +0200

    diary entry page start

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import DiaryEntry from "./components/DiaryEntry/DiaryEntry";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        {/* <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a> */}
+        <DiaryEntry />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/DiaryEntry/DiaryEntry.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@mui/base/Button";
+import "./DiaryEntry.css";
+
+const DiaryEntry = () => {
+  return (
+    <section>
+      <Button className="diaryEntryButton">Add entry</Button>
+    </section>
+  );
+};
+
+export default DiaryEntry;
